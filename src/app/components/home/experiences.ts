b296a836d8588dfc7576aeb1cd6e7b225acca1ea
SamuraiViking <nelson67@stolaf.edu>
2020-08-14 23:24:45 -0500

    update descriptions

--- src/app/components/home/experiences.ts
+++ src/app/components/home/experiences.ts
@@ -1,38 +1,39 @@
 export default [
-    {
-        "iframeURL": "https://www.youtube.com/embed/JVP9BW5NX70",
-        "title": "Multiplayer Game of Chess",
-        "subtitle": "Personal Project",
-        "class": "purple",
-        "description": "I love chess, and I love coding. So I coded chess!",
-        "site": "",
-        "github": "https://github.com/kevin-a-nelson/chess"
-    },
-    {
-        "iframeURL": "https://www.youtube.com/embed/dpiWVDZi9RY?start=57",
-        "title": "Point of Sale",
-        "subtitle": "Internship project at PrepNetwork",
-        "class": "blue",
-        "description": "I developed a POS for PrepNetwork from the ground up.",
-        "site": "",
-        "github": "",
-    },
-    {
-        "iframeURL": "https://www.youtube.com/embed/4jkqgRg3Rr0?start=2674",
-        "title": "Rate my Professor Course Planner",
-        "class": "pink",
-        "subtitle": "Capstone Project at Actualize Coding Bootcamp",
-        "description": "Rate my Professor is a website where profs are rated from 1 to 5. So I matched the courses at St. Olaf with their prof's respective rating from Rate my Professor.",
-        "site": "https://stocoursesplus.herokuapp.com",
-        "github": "https://github.com/kevin-a-nelson/StoCourses",
-    },
-    {
-        "iframeURL": "https://www.youtube.com/embed/3yCM8Sx7kwI",
-        "title": "Remember the Name",
-        "class": "blue2",
-        "subtitle": "Personal Project",
-        "description": "A mobile app that helps Stolaf Students remember the names of their peers",
-        "site": "https://expo.io/@nelson67/remember-names",
-        "github": "https://github.com/kevin-a-nelson/remember_names",
-    }
-]
+  {
+    iframeURL: "https://www.youtube.com/embed/JVP9BW5NX70",
+    title: "Multiplayer Game of Chess",
+    subtitle: "Personal Project",
+    class: "purple",
+    description: "Create/join a game and play chess against other players",
+    site: "",
+    github: "https://github.com/kevin-a-nelson/chess",
+  },
+  {
+    iframeURL: "https://www.youtube.com/embed/dpiWVDZi9RY?start=57",
+    title: "Point of Sale",
+    subtitle: "Internship project at a startup",
+    class: "blue",
+    description: "A POS that I developed from the ground up at a start up",
+    site: "",
+    github: "",
+  },
+  {
+    iframeURL: "https://www.youtube.com/embed/4jkqgRg3Rr0?start=2674",
+    title: "Rate my Professor Course Planner",
+    class: "pink",
+    subtitle: "Capstone Project at Actualize Coding Bootcamp",
+    description: "View St. Olaf colleges courses and their professors rating.",
+    site: "https://stocoursesplus.herokuapp.com",
+    github: "https://github.com/kevin-a-nelson/StoCourses",
+  },
+  {
+    iframeURL: "https://www.youtube.com/embed/3yCM8Sx7kwI",
+    title: "Remember the Name",
+    class: "blue2",
+    subtitle: "Personal Project",
+    description:
+      "A mobile app that helps Stolaf Students remember the names of their peers",
+    site: "https://expo.io/@nelson67/remember-names",
+    github: "https://github.com/kevin-a-nelson/remember_names",
+  },
+];
